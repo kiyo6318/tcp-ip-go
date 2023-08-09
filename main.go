c19a15fa51fdf33b1645589f9d7e4f4b3f69466a
kiyo <kiyonari6318@outlook.com>
2023-08-09 22:35:49 +0900

    Add: internet

--- main.go
+++ main.go
@@ -1,19 +1,42 @@
 package main
 
+// 　ネットワークインターフェース層
+
+// import (
+// 	"encoding/hex"
+// 	"fmt"
+
+// 	"github.com/kawa1214/tcp-ip-go/network"
+// )
+
+// func main() {
+// 	network, _ := network.NewTun()
+// 	network.Bind()
+
+// 	for {
+// 		pkt, _ := network.Read()
+// 		fmt.Print(hex.Dump(pkt.Buf[:pkt.N]))
+// 		network.Write(pkt)
+// 	}
+// }
+
+//  インターネット層
+
 import (
-	"encoding/hex"
 	"fmt"
 
+	"github.com/kawa1214/tcp-ip-go/internet"
 	"github.com/kawa1214/tcp-ip-go/network"
 )
 
 func main() {
 	network, _ := network.NewTun()
 	network.Bind()
+	ip := internet.NewIpPacketQueue()
+	ip.ManageQueues(network)
 
 	for {
-		pkt, _ := network.Read()
-		fmt.Print(hex.Dump(pkt.Buf[:pkt.N]))
-		network.Write(pkt)
+		pkt, _ := ip.Read()
+		fmt.Printf("IP Header: %+v\n", pkt.IpHeader)
 	}
 }
